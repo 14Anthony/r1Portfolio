2981e7e6f758a65c40db15ed0e3c0377111c9b46
Anthony Belcastro <belcastt@live.com>
2020-11-03 21:42:17 -0700

    reimage

--- src/products.js
+++ src/products.js
@@ -1,66 +1,58 @@
+
 const products = [
     {
-
-        name: 'THE FIRST PORTFOLIO',
-        image: '../images/One.jpg',
-        Language: 'html, Css',
+        _id: '1',
+        name: 'Create your First Project',
+        image: 'One',
         description:
-            'The first portfolio',
-        rating: 1.5,
+            'The knowledge I had starting off in this bootcamp was that my email, would sometimes ask me if I wanted to print in plain text html, and I did not know what that was',
+        rating: .5,
         numReviews: 12,
     },
     {
-
-        name: 'PASSWORD GENERATOR',
-        image: '../images/Two.jpg',
-        Language: 'html, Css, Java',
+        _id: '2',
+        name: 'My First Logic ',
+        image: 'Two',
         description:
-            'The first randomizer',
+            'This was the first time I said cool, when I made someting,  Understanding how to create something that generated random numbers',
         rating: 1.5,
-        numReviews: 12,
+        numReviews: 8,
     },
     {
-
-        name: 'TodoList',
-        image: '../images/todolist.jpg',
-        Language: 'html, Css, Javascript',
-        description:
-            'The first portfolio',
-        rating: 1.5,
-        numReviews: 12,
-    },
-    {
-
+        _id: '3',
         name: 'BelcastroWeather',
         image: '../images/BelcastroWeather.jpg',
-        Language: 'html, Css, Javascript, JQuery, Ajax, Weather APi',
         description:
-            'The first Ajax and api connection',
-        rating: 1.5,
+            'Here I actually began to understand what the instructor was saying when I heard, dynamic rendering, and jQuery and javascript became less foreign',
+        rating: 4,
+        numReviews: 20,
+    },
+    {
+        _id: '4',
+        name: 'Todo Lists, ',
+        image: '../images/todolist.jpg',
+        description:
+            'Stay organized in a pinch. Use this little guy to keep on task and runn thru getting a little bit better with JavaScript and ventured out of the single form 400 line code.',
+        rating: 3.5,
         numReviews: 12,
     },
     {
-
-        name: 'myVoice',
+        _id: '5',
+        name: 'MyVoice',
         image: '../images/myVoice.jpg',
-        Language: 'html, Css, Javascript, JQuery, Ajax, YouTube Api',
         description:
-            'Team Project 2',
-        rating: 1.5,
-        numReviews: 12,
+            'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music', category: 'Electronics',
+        rating: 4,
+        numReviews: 10,
     },
     {
-
-        name: 'First Sold Product',
+        _id: '6',
+        name: 'Hablador',
         image: '../images/hablador.jpg',
-        Language: 'html, Css, Javascript, Firebase',
-        description:
-            'The first portfolio',
-        rating: 1.5,
-        numReviews: 12,
+        description: 'My first sold project, it was mid re-entry, and Tucson COVID cases were going thru the roof, a not-for-profit needed a way to connect and communicate in chat rooms, throughout, they wanted to purchase teams from msft, and I told, I could create a chatroom form them and they could try it out.  They liked and then moved to more functionality and teams',
+        rating: 4.5,
+        numReviews: 22,
     },
-
 ]
 
-// module.export = products;
 export default products
